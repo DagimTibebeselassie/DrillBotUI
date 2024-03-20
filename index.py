af133b222b562cfef39ccbb12e8c4b41a5ab058b
Dagim Tibebeselassie <164085795+DagimTibebeselassie@users.noreply.github.com>
2024-03-20 14:34:03 -0400

    Update index.py

--- index.py
+++ index.py
@@ -54,6 +54,41 @@ app.layout = html.Div([
             id='grid',
             figure=add_grid()
         )
+    ]),
+    html.Div([
+            dcc.Input(
+                id='samples',
+                type='number',
+                value=1,  # Default value
+                min=0,
+                style={'height': '35','width': '45px', "border-radius":'50px'}
+            ),
+        ], style={'display': 'inline-block', 'margin-right': '20px'}),
+        html.Div([
+            dcc.Input(
+                id='minutes',
+                type='number',
+                value=0,  # Default value
+                min=0,
+                style={'height': '35','width': '45px', "border-radius":'50px'}
+            ),
+        ], style={'display': 'inline-block', 'margin-right': '20px'}),
+        html.Div([
+            dcc.Input(
+                id='seconds',
+                type='number',
+                value=0,  # Default value
+                min=0,
+                style={'height': '35','width': '45px', "border-radius":'50px'}
+            ),
+        ], style={'display': 'inline-block'}),
+    html.Div([
+        html.Div('Samples', style={'position':'absolute'}),
+        html.Div('Minutes', style={'position':'absolute','margin-left': '75px'}),
+        html.Div('Seconds', style={'position':'absolute', 'margin-left': '149px'}),
+    ]),
+    html.Div([
+        html.Button()
     ])
 ])
 
